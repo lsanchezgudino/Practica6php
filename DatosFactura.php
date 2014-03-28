e093b916280304e349e36e5f7bd03bad05ac0a03
lsanchezgudino <3001luly@gmail.com>
2014-03-27 18:20:19 -0600

    Create DatosFactura.php

---
+++ DatosFactura.php
@@ -0,0 +1,26 @@
+<form action="Factura.php" method="POST">
+<table border="1">
+	<tr>
+			<td></td>
+			<td>Nombre</td>
+			<td>Precio</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td><input type="text" size="10" maxlength="10" id="pro1" name="pro1"><br/></td>
+			<td><input type="text" size="10" maxlength="10" id="pro1Pre" name="pro1Pre"></td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td><input type="text" size="10" maxlength="10" id="pro2" name="pro2"><br/></td>
+			<td><input type="text" size="10" maxlength="10" id="pro2Pre" name="pro2Pre"></td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td><input type="text" size="10" maxlength="10" id="pro3" name="pro3"><br/></td>
+			<td><input type="text" size="10" maxlength="10" id="pro3Pre" name="pro3Pre"></td>
+		</tr>
+</table>
+<input type="submit" value="Enviar">
+</form>
+<?php
